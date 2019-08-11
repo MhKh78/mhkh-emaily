7823476af0091ddd3c4a1620f2cbe2f3ad9dc3d7
MhKh78 <mhkh431@gmail.com>
2019-08-11 18:29:18 +0430

    First Bug Fixed

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '127.0.0.1', () => {
-  console.log(`Listening on http://127.0.0.1:${PORT} ....!!!`);
+app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT} ....!!!`);
 });
