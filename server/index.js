103fe7296153966bca9c964ea0fe1ba3926ec31c
MhKh78 <mhkh431@gmail.com>
2019-08-19 14:53:47 +0430

    Passport added

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
+const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.google.client_id,
+      clientSecret: keys.google.client_secret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on http://127.0.0.1:${PORT} ....!!!`);
+});
